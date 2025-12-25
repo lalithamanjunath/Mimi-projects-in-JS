bf1c93da892a2226bcfb84a0f70311ac3d867e5d
lalithamanjunath <lalithamanjunath05@gmail.com>
2025-12-25 13:38:08 +0530

    BMI Calculator

--- pro2.js
+++ pro2.js
@@ -16,7 +16,7 @@ calculateBtn.addEventListener("click", () => {
 if (!height || !weight || height<= 0 || weight<= 0 ) {
     resultDiv.style.color= "red";
     resultDiv.textContent= "Please enter valid height and weight of yours..!!";
-    return
+    return;
 }
 // !height --> means empty or NaN
 
@@ -28,36 +28,35 @@ let category = "";
 let color = "";
 let advice = "";
 
-if (bmi < 18.5){
+if (bmi < 18.5) {
     category = "Underweight";
     color = "orange";
     advice = "Eat nutritious food and maintain a healthy diet.";
-} else if (bmi < 25) {
+  } else if (bmi < 25) {
     category = "Normal";
     color = "green";
     advice = "Great job! Maintain your healthy lifestyle.";
-} else if (bmi < 30) {
-    category = "Over-weight";
+  } else if (bmi < 30) {
+    category = "Overweight";
     color = "darkorange";
     advice = "Exercise regularly and balance your diet.";
-} else {
+  } else {
     category = "Obese";
     color = "red";
     advice = "Consult a healthcare professional.";
-}
+  }
 
-resultDiv.style.color = color;
-resultDiv.innerHTML =`
-So your <br>
-BMI: ${bmi} <br>
-Category: ${category} <br>
-<small>${advice}</small>
-`;
+  resultDiv.style.color = color;
+  resultDiv.innerHTML = `
+    So your<br>
+    BMI: ${bmi} <br>
+    Category: ${category} <br>
+    <small>${advice}</small>
+  `;
 });
 
 resetBtn.addEventListener("click", () => {
-    heightInput.value = "";
-    weightInput.value = "";
-    resultDiv.textContent = "";
+  heightInput.value = "";
+  weightInput.value = "";
+  resultDiv.textContent = "";
 });
-
